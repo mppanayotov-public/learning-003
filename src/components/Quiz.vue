93f65f801113bb6c677612b255c106aec5afb0d8
milenpanayotov <milenpanayotov@2create.studio>
2019-11-25 18:08:41 +0200

    Add timer - work in progress

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -4,35 +4,7 @@
 			<template v-slot:content>
 				<div class="modal-content">
 					<div class="icon">
-						<svg id="Lock_Icon" data-name="Lock Icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="106" height="106" viewBox="0 0 106 106">
-						  <defs>
-						    <clipPath id="clip-path">
-						      <path id="Mask" d="M45.455,32H0V22.857A22.772,22.772,0,0,1,6.659,6.7,22.624,22.624,0,0,1,38.8,6.7a22.772,22.772,0,0,1,6.659,16.16V32ZM22.5,8A14.264,14.264,0,0,0,8,21.981V31H37V21.981A14.264,14.264,0,0,0,22.5,8Z" transform="translate(30.5 4.5)" fill="#d1d1d1"/>
-						    </clipPath>
-						    <clipPath id="clip-path-2">
-						      <path id="Mask-2" data-name="Mask" d="M61.835,61.835h-53A8.844,8.844,0,0,1,0,53V8.832A8.842,8.842,0,0,1,8.832,0h53a8.842,8.842,0,0,1,8.832,8.832V53A8.844,8.844,0,0,1,61.835,61.835ZM53,26.5a4.416,4.416,0,1,0,4.416,4.416A4.421,4.421,0,0,0,53,26.5Zm-17.665,0a4.416,4.416,0,1,0,4.416,4.416A4.421,4.421,0,0,0,35.334,26.5Zm-17.669,0a4.416,4.416,0,1,0,4.42,4.416A4.421,4.421,0,0,0,17.665,26.5Z" transform="translate(0 0)" fill="none"/>
-						    </clipPath>
-						    <clipPath id="clip-path-3">
-						      <rect id="Black" width="181.083" height="181.083" fill="#9b9b9b"/>
-						    </clipPath>
-						  </defs>
-						  <path id="Mask-3" data-name="Mask" d="M45.455,32H0V22.857A22.772,22.772,0,0,1,6.659,6.7,22.624,22.624,0,0,1,38.8,6.7a22.772,22.772,0,0,1,6.659,16.16V32ZM22.5,8A14.264,14.264,0,0,0,8,21.981V31H37V21.981A14.264,14.264,0,0,0,22.5,8Z" transform="translate(30.5 4.5)" fill="#d1d1d1"/>
-						  <g id="Group_4" data-name="Group 4" transform="translate(31 60)">
-						    <circle id="Oval" cx="6" cy="6" r="6" fill="#d8d8d8"/>
-						    <circle id="Oval_Copy" data-name="Oval Copy" cx="6" cy="6" r="6" transform="translate(16)" fill="#d8d8d8"/>
-						    <circle id="Oval_Copy_2" data-name="Oval Copy 2" cx="6" cy="6" r="6" transform="translate(34)" fill="#d8d8d8"/>
-						  </g>
-						  <g id="ic_https">
-						    <g transform="translate(17.667 35.333)">
-						      <path id="Mask-4" data-name="Mask" d="M61.835,61.835h-53A8.844,8.844,0,0,1,0,53V8.832A8.842,8.842,0,0,1,8.832,0h53a8.842,8.842,0,0,1,8.832,8.832V53A8.844,8.844,0,0,1,61.835,61.835ZM53,26.5a4.416,4.416,0,1,0,4.416,4.416A4.421,4.421,0,0,0,53,26.5Zm-17.665,0a4.416,4.416,0,1,0,4.416,4.416A4.421,4.421,0,0,0,35.334,26.5Zm-17.669,0a4.416,4.416,0,1,0,4.42,4.416A4.421,4.421,0,0,0,17.665,26.5Z" transform="translate(0 0)" fill="none"/>
-						      <g clip-path="url(#clip-path-2)">
-						        <g id="_Color" data-name="🎨 Color" transform="translate(-61.833 -75.083)">
-						          <rect id="Black-2" data-name="Black" width="181.083" height="181.083" fill="#9b9b9b"/>
-						        </g>
-						      </g>
-						    </g>
-						  </g>
-						</svg>
+						<img src="../assets/lock.png" alt="" width="106" height="106">
 					</div><!-- /.icon -->
 					
 					<h2>An access code is required to start.</h2>
@@ -67,6 +39,10 @@
 				<div class="box">
 					<div class="timer">
 						Time Remaining
+
+						<div class="timer__time js-timer">
+							05:00:00
+						</div><!-- /.timer__time -->
 					</div><!-- /.progress -->
 				</div><!-- /.box -->
 			</div><!-- /.cols -->
@@ -126,6 +102,7 @@ export default {
 			this.updateQuestionStatus();
 			this.quizStart = false;
 			this.quizQuestions = true;
+			this.startTimer();
 		},
 		quizPrev: function() {
 			const activeIndex = this.questions.findIndex(question => question.isActive);
@@ -192,6 +169,20 @@ export default {
 			calculateAnsweredAmount();
 			progress = answeredAmount.length / questionsAmount * 100;
 			progressBar.style.transform = "translateX(" + progress + "%)";
+		},
+		startTimer: function() {
+			const timer = document.querySelector('.js-timer');
+			const duration = 5 * 60 * 60;
+
+			setInterval(function () {
+				let minutes = parseInt(duration / 60, 10)
+				let seconds = parseInt(duration % 60, 10);
+
+				minutes = minutes < 10 ? "0" + minutes : minutes;
+				seconds = seconds < 10 ? "0" + seconds : seconds;
+
+				timer.textContent = minutes + ":" + seconds;
+			}, 1000);
 		}
 	},
 	computed: {
